71a8d30ea6050fbefd26dd1ba7c6ecaef02ec338
Aviraj19 <73528868+Aviraj19@users.noreply.github.com>
2022-05-05 16:53:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+status1=""
 function preload() {
 img=loadImage("dog_cat.jpg")
 }
 function setup() {
     canvas=createCanvas(800,450)
     canvas.center();
+    objectdetector=ml5.objectDetector("cocossd", modelloaded)
 }
 function draw() {
     image(img,0,0,800,450)
@@ -14,5 +16,22 @@ function draw() {
     stroke("red")
     rect(55,55,400,350)
     strokeWeight(3)
+    fill("black")
+    text("Cat",400,50)
+    noFill()
+    rect(400,75,350,350)
 }
+ function modelloaded() {
+     console.log("Model is loaded")
+     status1=true
+     objectdetector.detect(img,getresult)
+ }
+ function getresult(error,results) {
+     if (error) {
+         console.log("error")
+     }
+     else {
+        console.log(results)
+     }
+ }
  
